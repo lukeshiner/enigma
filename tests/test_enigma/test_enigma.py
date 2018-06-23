9d595df00917817e35496891e970665a8b076b5a
Luke Shiner <luke@lukeshiner.com>
2018-06-23 14:46:17 +0100

    Add tests for Enigma class.

---
+++ tests/test_enigma/test_enigma.py
@@ -0,0 +1,39 @@
+"""Tests for the enigma module."""
+
+from enigma import Enigma
+
+
+class TestEnigma:
+    """Tests for the enigma class."""
+
+    def get_enigma(
+            self,
+            rotors=[
+                {
+                    'wiring': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+                    'position': 'A',
+                    'ring_setting': 1,
+                    'turnover_positions': ['R'],
+                },
+                {
+                    'wiring': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+                    'position': 'A',
+                    'ring_setting': 1,
+                    'turnover_positions': ['F'],
+                },
+                {
+                    'wiring': 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+                    'position': 'A',
+                    'ring_setting': 1,
+                    'turnover_positions': ['W'],
+                },
+            ],
+            reflector='YRUHQSLDPXNGOKMIEBFZCWVJAT',
+            plugboard=[]):
+        """Test enigma object can be created."""
+        return Enigma(rotors=rotors, reflector=reflector, plugboard=plugboard)
+
+    def test_enigma_default_encoding(self):
+        """Test enigma encoding with default setup."""
+        enigma = self.get_enigma()
+        assert enigma.encode('HELL OWOR LD') == 'ILBD AAMT AZ'
